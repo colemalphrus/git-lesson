6a86ec67619e13e4a4638d28bd6a55b2620cf05a
cole malphrus <cole@malphrus.tech>
2021-04-07 19:42:39 -0600

    added for loop

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def main():
     print("Hello Git")
+    text = "this is the thing to change"
+    print(text)
+    x = 0
+    for i in range(25):
+        x += i
+
+    print(i)
 
 
 if __name__ == "__main__":
